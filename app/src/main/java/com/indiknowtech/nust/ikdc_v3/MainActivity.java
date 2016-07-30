e6a6bd56175553bb66247dfe1bd869f058bbb297
Donovan Maasz <donovan@IT-Tech.HP-PROBOOK>
2016-07-30 14:33:19 +0100

    recommit

---
+++ app/src/main/java/com/indiknowtech/nust/ikdc_v3/MainActivity.java
@@ -0,0 +1,29 @@
+package com.indiknowtech.nust.ikdc_v3;
+
+import android.annotation.SuppressLint;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.View;
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.hide();
+        }
+    }
+
+}
