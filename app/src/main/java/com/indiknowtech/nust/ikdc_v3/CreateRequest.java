30401c17b9207a7302a1ecad067a25cdcf4e4831
donovan <dmdmaasz6.reg@outlook.com>
2016-07-31 19:19:46 +0100

    Donovan - Show/Hide Fragments

--- app/src/main/java/com/indiknowtech/nust/ikdc_v3/CreateRequest.java
+++ app/src/main/java/com/indiknowtech/nust/ikdc_v3/CreateRequest.java
@@ -2,6 +2,7 @@ package com.indiknowtech.nust.ikdc_v3;
 
 //import android.support.v4.app.FragmentManager;
 //import android.support.v4.app.FragmentTransaction;
+import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.content.Intent;
@@ -11,7 +12,11 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 
+import com.indiknowtech.nust.ikdc_v3.fragments.AudioFragment;
+import com.indiknowtech.nust.ikdc_v3.fragments.DrawingFragment;
 import com.indiknowtech.nust.ikdc_v3.fragments.ImageFragment;
+import com.indiknowtech.nust.ikdc_v3.fragments.TextFragment;
+import com.indiknowtech.nust.ikdc_v3.fragments.VideoFragment;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
@@ -19,6 +24,15 @@ import com.indiknowtech.nust.ikdc_v3.fragments.ImageFragment;
  */
 public class CreateRequest extends AppCompatActivity {
 
+    FragmentManager fragmentManager;
+    FragmentTransaction fragmentTransaction;
+    ImageFragment imageFragment;
+    VideoFragment videoFragment;
+    AudioFragment audioFragment;
+    TextFragment textFragment;
+    DrawingFragment drawingFragment;
+
+    int tab_flag = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,39 +46,141 @@ public class CreateRequest extends AppCompatActivity {
         }
         //TODO: Determine the indigenous loading symbol
 
-        //***** Load ImageFragment Starts
+        fragmentManager = getFragmentManager();
+        imageFragment = new ImageFragment();
+        videoFragment = new VideoFragment();
+        audioFragment = new AudioFragment();
+        textFragment = new TextFragment();
+        drawingFragment = new DrawingFragment();
+
+        //***** Load ImageFragment [Flag = 1] Starts
         View.OnClickListener load_imagefragment = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                FragmentManager fragmentManager = getFragmentManager();
-                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
 
-                ImageFragment fragment = new ImageFragment();
-                fragmentTransaction.add(R.id.container, fragment);
-                fragmentTransaction.commit();
+                switch (tab_flag) {
+                    case 0:
+                        fragmentTransaction = fragmentManager.beginTransaction();
+
+                        fragmentTransaction.add(R.id.container, imageFragment);
+                        fragmentTransaction.commit();
+
+                        tab_flag = 1;
+                        break;
+                    case 2:
+                        showHideFragment(imageFragment, audioFragment);
+                        break;
+                    case 3:
+                        showHideFragment(imageFragment, videoFragment);
+                        break;
+                    case 4:
+                        showHideFragment(imageFragment, textFragment);
+                        break;
+                    case 5:
+                        showHideFragment(imageFragment, drawingFragment);
+                        break;
+                }
             }
         };
-        ImageButton viewRequestButton = (ImageButton) findViewById(R.id.eye);
-        viewRequestButton.setOnClickListener(load_imagefragment);
+        ImageButton imageFragmentButton = (ImageButton) findViewById(R.id.eye);
+        imageFragmentButton.setOnClickListener(load_imagefragment);
         //****  Load ImageFragment Ends
 
-        //**** Load Audio Fragment
+        //==========================================================================================
 
-        //**** Load Audio Fragment
+        //**** Load Audio Fragment [Flag = 2] Starts
+        View.OnClickListener load_audiofragment = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
+                switch (tab_flag) {
+                    case 0:
+                        fragmentTransaction = fragmentManager.beginTransaction();
+
+                        fragmentTransaction.add(R.id.container, audioFragment);
+                        fragmentTransaction.commit();
+
+                        tab_flag = 2;
+                        break;
+                }
+
+            }
+        };
+        final ImageButton audioFragmentButton = (ImageButton) findViewById(R.id.mouth);
+        audioFragmentButton.setOnClickListener(load_audiofragment);
+        //**** Load Audio Fragment Ends
+
+        //==========================================================================================
+
+        //**** Load Video Fragment [Flag = 3] Starts
+        View.OnClickListener load_videofragment = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                switch (tab_flag) {
+                    case 0:
+                        fragmentTransaction = fragmentManager.beginTransaction();
+
+                        fragmentTransaction.add(R.id.container, videoFragment);
+                        fragmentTransaction.commit();
+
+                        tab_flag = 3;
+                        break;
+                    case 1:
+                        fragmentTransaction.hide(audioFragment);
+                }
+
+            }
+        };
+        ImageButton videoFragmentButton = (ImageButton) findViewById(R.id.feet);
+        videoFragmentButton.setOnClickListener(load_videofragment);
         //**** Load Video Fragment
 
-        //**** Load Video Fragment
+        //==========================================================================================
 
         //**** Load Text Fragment
 
         //**** Load Text Fragment
 
-        //**** Load Drawing Fragment
-        
+        //==========================================================================================
+
         //**** Load Drawing Fragment
 
+        //**** Load Drawing Fragment
+
+        //==========================================================================================
     }
 
+    private boolean showHideFragment (Fragment hiddenFragment, Fragment shownFragment){
+
+        try {
+
+            fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.hide(shownFragment);
+            fragmentTransaction.commit();
+
+            if(!hiddenFragment.isAdded()){
+
+                fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.add(R.id.container, hiddenFragment);
+                fragmentTransaction.commit();
+
+            }else
+            {
+
+                fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.show(hiddenFragment);
+                fragmentTransaction.commit();
+
+            }
+
+            return true;
+
+        } catch (Exception e){
+
+            return false;
+
+        }
+    }
 
 }
